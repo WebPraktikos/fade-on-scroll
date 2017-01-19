ee3b096d4decd84e4ff209fd639fb5886dce7b7e
WebPraktikos <hi@webpraktikos.com>
2017-01-19 18:24:10 +0100

    Syntax changes

--- fade-on-scroll.js
+++ fade-on-scroll.js
@@ -4,38 +4,38 @@
  * @version 1.0.1
  * @license MIT License
  */
-;(function( window ) {
+;(function(window) {
   'use strict';
 
   /**
-   * Outer function which remembers shared "page container" for all instances.
-   * @param {string} _page - Element on which a scroll listener is attached.
+   * Outer function that holds shared "page container" for all instances.
+   * @param {string} _page - Element on which the scroll listener is attached.
    * @return {function} - Inner function.
    */
-  window.FadeOnScroll = function( _page ) {
+  window.FadeOnScroll = function(_page) {
     var page = _page || 'window';
 
     /**
-     * Changes opacity of the element.
+     * Change opacity of the element.
      * @param  {array} _elementsArr - Elements and its options.
      */
-    var doInstance = function( _elementsArr ) {
+    var doInstance = function(_elementsArr) {
       var _arguments = arguments;
 
       /**
        * Calculates opacity for the specified element based on its position.
-       * @param  {string} _element - Element that will change opacity.
+       * @param  {string} _element - Element which will change its opacity.
        * @param  {object} _options - Custom options for the element.
        * @return {array} - Element and its opacity.
        */
-      var getElOpct = function( _element , _options ) {
-        var contex , toEnd;
-        var element = document.querySelectorAll( _element );
+      var getElOpct = function(_element, _options) {
+        var contex, toEnd;
+        var element = document.querySelectorAll(_element);
 
         /** Merges default options with the passed options. */
-        var options = extend( {} , defaultOptions );
-        if ( _options ) {
-          extend( options, _options );
+        var options = extend({}, defaultOptions);
+        if (_options) {
+          extend(options, _options);
         }
 
         /** Default options. */
@@ -51,33 +51,33 @@
           elTopFromVPBtm = inrVpHeight - elTopFromVPTop,
           elBtmFromVPTop = elTopFromVPTop + elHeight;
 
-        if (  options.waypoint.indexOf( '%' ) > -1 ) {
-          var inrVpHeightFraction = 100 / parseInt( options.waypoint , 10 );
-          contex = inrVpHeight / inrVpHeightFraction.toFixed( 2 );
+        if (options.waypoint.indexOf('%') > -1) {
+          var inrVpHeightFraction = ( 100 / parseInt(options.waypoint, 10) );
+          contex = ( inrVpHeight / inrVpHeightFraction.toFixed(2) );
 
-        } else if ( options.waypoint.indexOf( 'px' ) > -1 ) {
-          contex = parseInt( options.waypoint , 10 );
+        } else if (options.waypoint.indexOf('px') > -1) {
+          contex = parseInt(options.waypoint, 10);
 
-        } else if ( options.waypoint === 'self' ) {
+        } else if (options.waypoint === 'self') {
           contex = elHeight;
         }
 
-        if ( options.direction === 'top' ) {
+        if (options.direction === 'top') {
           toEnd = elBtmFromVPTop;
         }
 
-        if ( options.direction === 'bottom' ) {
+        if (options.direction === 'bottom') {
           toEnd = elTopFromVPBtm;
         }
 
-        if ( options.direction === 'both' ) {
-          var toTop = elBtmFromVPTop / contex;
-          toEnd = ( toTop <= 1 ) ? elBtmFromVPTop : elTopFromVPBtm;
+        if (options.direction === 'both') {
+          var toTop = (elBtmFromVPTop / contex);
+          toEnd = ( (toTop <= 1) ? elBtmFromVPTop : elTopFromVPBtm );
         }
 
-        var opct = ( toEnd / contex ).toFixed( 2 );
+        var opct = (toEnd / contex).toFixed(2);
 
-        return [ _element , opct ];
+        return [_element, opct];
       }
 
       /**
@@ -86,26 +86,26 @@
        */
       var getElOpctArr = function() {
 
-        if ( Object.prototype.toString.call( _arguments[0] ) === '[object Array]' ) {
+        if (Object.prototype.toString.call(_arguments[0] ) === '[object Array]') {
 
-          return _elementsArr.map(function( item ) {
+          return _elementsArr.map(function(item) {
 
-            return getElOpct( item[0] , item[1] );
+            return getElOpct(item[0], item[1]);
           });
 
         } else {
 
-          return [ getElOpct( _arguments[0] , _arguments[1] ) ];
+          return [getElOpct(_arguments[0], _arguments[1])];
         }
       }
 
       /** Changes opacity on the specified element or elements. */
       var changeOpct = function() {
 
-        getElOpctArr().forEach(function( item ) {
+        getElOpctArr().forEach(function(item) {
 
-          Array.prototype.slice.call( document.querySelectorAll( item[0] ) )
-            .forEach(function( subItem ) {
+          Array.prototype.slice.call( document.querySelectorAll(item[0]) )
+            .forEach(function(subItem) {
               subItem.style.opacity = item[1]
             })
         })
@@ -115,10 +115,10 @@
       changeOpct();
 
       var onScroll = function() {
-        var container = ( page === 'window' ) ? window : document.querySelector( page );
+        var container = (page === 'window') ? window : document.querySelector(page);
 
-        container.addEventListener( 'scroll' , function() {
-          window.requestAnimationFrame( changeOpct );
+        container.addEventListener('scroll', function() {
+          window.requestAnimationFrame(changeOpct);
         })
       }
 
@@ -149,9 +149,9 @@
    ****/
 
   /** Utility function for object merging. */
-  function extend( a, b ) {
-    for ( var key in b ) {
-      if ( b.hasOwnProperty( key ) ) {
+  function extend(a, b) {
+    for (var key in b) {
+      if (b.hasOwnProperty(key)) {
         a[key] = b[key];
       }
     }
@@ -185,5 +185,4 @@
         clearTimeout( id );
       };
   }());
-
-})( window );
+})(window);
